d0a9f37dc1b705f4274ed1ea0ecc14465ad31f61
Patrick Ndifon <pmndifon@gmail.com>
2017-11-23 16:18:55 +0100

    Install test dependencies mocha and chai

--- server/app.js
+++ server/app.js
@@ -1,16 +1,14 @@
 import express from 'express';
 
+
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
   res.json({
     response: 'API Works'
-  })
+  });
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`server on port ${app.get('port')}`);
-});
 
+export default app;

be0e2b6a0c298757bb195e5da544c113548b6e10
Patrick Ndifon <pmndifon@gmail.com>
2017-11-24 10:49:12 +0100

    Modify 'eventsdb'to match 'eventsController'

--- server/api/models/eventdb.js
+++ server/api/models/eventdb.js
@@ -1,33 +1,33 @@
 const eventdb = [
   {
-    eventId: 1,
+    id: 1,
     name: 'AIDS Conference',
     eventType: "Other",
     dateBegin: '11-30-2017',
     dateEnd: '11-30 - 2017',
     centerName: 'Obong Calabar', 
-    details: "Lorem ipsum dolor sit amet"
+    description: "Lorem ipsum dolor sit amet"
   },
 
   {
-    eventId: 2,
+    id: 2,
     name: 'WhoKes Comedy Jam',
     eventType: "Team Outing/Party",
     dateBegin: '11-30-2017',
     dateEnd: '11-30-2017',
     centerName: 'Jasper',
-    details: "Lorem ipsum dolor sit amet"
+    description: "Lorem ipsum dolor sit amet"
   },
 
   {
-  eventId: 3,
-  name: 'Town Hall Meeting',
-  eventType: "Corporate",
-  dateBegin: '12-10-2017',
-  dateEnd: '12-13 - 2017',
-  centerName: 'Obong Calabar',
-  details: "Lorem ipsum dolor sit amet"
-  },
+    id: 3,
+    name: 'Town Hall Meeting',
+    eventType: "Corporate",
+    dateBegin: '12-10-2017',
+    dateEnd: '12-13 - 2017',
+    centerName: 'Obong Calabar',
+    description: "Lorem ipsum dolor sit amet"
+    },
 ];
 
 export default eventdb;
